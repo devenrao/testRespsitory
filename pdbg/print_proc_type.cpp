8ea70b237a48e384a8a4ef5b69de77b8ae92e962
Marri Devender Rao <devenrao@in.ibm.com>
2023-01-05 07:42:32 -0600

    Application to print proc type of all the procs

---
+++ pdbg/print_proc_type.cpp
@@ -0,0 +1,83 @@
+#include <libpdbg.h>
+#include <vector>
+#include <iostream>
+#include <string.h>
+
+void pdbgLogCallback(int, const char* fmt, va_list ap)
+{
+    va_list vap;
+    va_copy(vap, ap);
+    std::vector<char> logData(1 + std::vsnprintf(nullptr, 0, fmt, ap));
+    std::vsnprintf(logData.data(), logData.size(), fmt, vap);
+    va_end(vap);
+    std::string logstr(logData.begin(), logData.end());
+    std::cout << "PDBG:" << logstr << std::endl;
+}
+
+uint8_t getTrgtType(pdbg_target* i_trgt)
+{
+    uint8_t attr = 0;
+    pdbg_target_get_attribute(i_trgt, "ATTR_TYPE", 1, 1, &attr);
+    return attr;
+}
+
+pdbg_target* getPibTrgt(pdbg_target* i_procTrgt)
+{
+    // The input target must be a processor.
+    std::cout << "TYPE_PROC " << "getTrgtType " 
+        << static_cast<uint16_t>(getTrgtType(i_procTrgt)) << std::endl;
+
+    char path[16];
+    sprintf(path, "/proc%d/pib", pdbg_target_index(i_procTrgt));
+
+    // Return the pib target.
+    pdbg_target* pibTrgt = pdbg_target_from_path(nullptr, path);
+
+    return pibTrgt;
+}
+
+int main()
+{
+    constexpr auto devtree = "/var/lib/phosphor-software-manager/pnor/rw/DEVTREE";
+
+    // PDBG_DTB environment variable set to CEC device tree path
+    if (setenv("PDBG_DTB", devtree, 1))
+    {
+        std::cout << "Failed to set PDBG_DTB: " << strerror(errno) << std::endl;
+        return 0;
+    }
+
+    constexpr auto PDATA_INFODB_PATH = "/usr/share/pdata/attributes_info.db";
+    // PDATA_INFODB environment variable set to attributes tool  infodb path
+    if (setenv("PDATA_INFODB", PDATA_INFODB_PATH, 1))
+    {
+        std::cout << "Failed to set PDATA_INFODB: ({})" << strerror(errno) << std::endl;
+        return 0;
+    }
+
+    // initially set the pdbg backend to sbefifo
+    std::cout << "***Start with SBEFIFO as backend ******" << std::endl;
+    if (!pdbg_set_backend(PDBG_BACKEND_SBEFIFO, NULL)) 
+    {
+        std::cout << "Failed to set pdbg backend to fifo" << std::endl;
+    }
+
+    //initialize the targeting system 
+    if (!pdbg_targets_init(NULL))
+    {   
+        std::cout << "pdbg_targets_init failed" << std::endl;
+        return 0;
+    }
+
+    // set log level and callback function
+    pdbg_set_loglevel(PDBG_DEBUG);
+    pdbg_set_logfunc(pdbgLogCallback);
+
+    struct pdbg_target* procTarget;
+    pdbg_for_each_class_target("proc", procTarget)
+    {
+        auto index = std::to_string(pdbg_target_index(procTarget));
+        getPibTrgt(procTarget);
+    }
+    return 1;
+}
---
+++ pdbg/switch_pdbg_backend.C
@@ -0,0 +1,94 @@
+#include <libpdbg.h>
+#include <vector>
+#include <iostream>
+#include <string.h>
+
+#include <attributes_info.H>
+#include <ekb/chips/p10/procedures/hwp/perv/p10_extract_sbe_rc.H>
+#include <ekb/hwpf/fapi2/include/return_code_defs.H>
+
+void pdbgLogCallback(int, const char* fmt, va_list ap)
+{
+    va_list vap;
+    va_copy(vap, ap);
+    std::vector<char> logData(1 + std::vsnprintf(nullptr, 0, fmt, ap));
+    std::vsnprintf(logData.data(), logData.size(), fmt, vap);
+    va_end(vap);
+    std::string logstr(logData.begin(), logData.end());
+    std::cout << logstr << std::endl;
+}
+static bool flag = false;
+void myGetScom(enum pdbg_backend backend)
+{
+    // initially set the pdbg backend to sbefifo
+    if (!pdbg_set_backend(backend, "p10"))
+    {
+        std::cout << "Failed to set pdbg backend to " << backend << std::endl;
+    }
+
+    constexpr auto devtree = "/var/lib/phosphor-software-manager/pnor/rw/DEVTREE";
+    // PDBG_DTB environment variable set to CEC device tree path
+    if (setenv("PDBG_DTB", devtree, 1)) 
+    {   
+        std::cout << "Failed to set PDBG_DTB: " << strerror(errno) << std::endl;
+    }   
+
+    constexpr auto PDATA_INFODB_PATH = "/usr/share/pdata/attributes_info.db";
+    // PDATA_INFODB environment variable set to attributes tool  infodb path
+    if (setenv("PDATA_INFODB", PDATA_INFODB_PATH, 1)) 
+    {   
+        std::cout << "Failed to set PDATA_INFODB: ({})" << strerror(errno) << std::endl;
+    }   
+
+    //initialize the targeting system 
+    if (!pdbg_targets_init(NULL))
+    {   
+        std::cout << "pdbg_targets_init failed" << std::endl;
+    }
+    if(!flag) 
+    {
+        pdbg_target_probe_all(pdbg_target_root());
+        flag = true;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    struct pdbg_target* procTarget;
+    pdbg_for_each_class_target("proc", procTarget)
+    {
+        static const uint64_t addr = 0x10012ull;
+        fapi2::buffer<uint64_t> l_data64;
+        std::cout << " before devender0113 proc scom data received is " << std::hex <<  l_data64 << std::endl;
+        int ret = getScom(static_cast<const fapi2::Target<fapi2::TARGET_TYPE_PROC_CHIP> >(procTarget), addr, l_data64);
+        std::cout << ret << " after devender0113 proc scom data received is " << std::hex <<  l_data64 << std::endl;
+        return;
+    }
+}
+
+int main()
+{
+    // set log level and callback function
+    pdbg_set_loglevel(PDBG_ERROR);
+    pdbg_set_logfunc(pdbgLogCallback);
+
+    // initially set the pdbg backend to sbefifo
+    std::cout << "DEVENDER0114 *****before GetScom with KERNEL as backend ****"<< std::endl;
+    myGetScom(PDBG_BACKEND_KERNEL);
+    std::cout << "DEVENDER0114 *****after GetScom with KERNEL as backend ****"<< std::endl;
+
+    //std::cout << "Reset the device tree root " << std::endl;
+    pdbg_reset_dt_root();
+
+    std::cout << "****DEVENDER0114 before GetScom with SBEFIFO as backend ***"<< std::endl;
+    myGetScom(PDBG_BACKEND_SBEFIFO);
+    std::cout << "****DEVENDER0114 before GetScom with SBEFIFO as backend ***"<< std::endl;
+    //std::cout << "Reset the device tree root " << std::endl;
+   // pdbg_reset_dt_root();
+
+    //std::cout << "***GetScom with SBEFIFO as backend ****"<< std::endl;
+    //myGetScom(PDBG_BACKEND_SBEFIFO);
+    return 1;
+}
